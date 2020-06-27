27e891bc736e4e74c674aafc6763548210d9bbfe
Paulalbo <65129276+Paulalbo@users.noreply.github.com>
2020-06-27 14:41:21 +0200

    added some information per flight

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,44 +1,60 @@
 export const products = [
     {
       img: 'https://images.pexels.com/photos/3393450/pexels-photo-3393450.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      name: 'BALI',
+      destination: 'BALI',
       price: 98.00,
+      duration: '5 days',
+      startdate: '31.06.2020',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },{
       img: 'https://images.ctfassets.net/knd0dn3kghk7/1Dvdu8J3lyQ6SkgUw4CgkE/1be1b4d4f10c0dbb458dfef7e1433593/getty-120225463.jpg?w=946&h=532&fit=thumb',
-      name: 'UBUD',
+      destination: 'UBUD',
       price: 45.00,
+      duration: '7 days',
+      startdate: '15.08.2020',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },
     {
       img: 'https://q-cf.bstatic.com/images/hotel/max1024x768/216/216403713.jpg',
-      name: 'KUTA',
+      destination: 'KUTA',
       price: 89.00,
+      duration: '14 days',
+      startdate: '14.09.2020',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },{
       img: 'https://dsvsbigncb06y.cloudfront.net/site/diving/indonesia/liveaboard-indonesia-lombok-volcano-xxl.jpg',
-      name: 'LOMBOK',
+      destination: 'LOMBOK',
       price: 29.00,
+      duration: '3 days',
+      startdate: '31.07.2020',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },{
       img: 'https://www.wohin-und-wann.de/site/images/illustration/raja-ampat-islands.jpg',
-      name: 'RAJA AMPAT',
+      destination: 'RAJA AMPAT',
       price: 45.00,
+      duration: '5 days',
+      startdate: '01.08.2020',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },{
       img: 'https://www.bestereisezeit.at/uploads/2018/05/beste-reistijd-jakarta-indonesie-1200x675.jpg',
-      name: 'JAKARTA',
+      destination: 'JAKARTA',
       price: 59.00,
+      duration: '10 days',
+      startdate: '15.07.2020',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },{
       img: 'https://www.asien-reiseprofi.de/wp-content/uploads/Belmond_Jimbaran_Puri_Bali_Nelayan_Restaurant_Puri_Bar-1680x913.jpg',
-      name: 'JIMBARAN',
+      destination: 'JIMBARAN',
       price: 8.90,
+      duration: '8 days',
+      startdate: '01.07.2020',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },{
       img: 'https://www.orangesmile.com/common/img_cities_w680/denpasar--2676772-6.jpg',
-      name: 'DENPASAR',
+      destination: 'DENPASAR',
       price: 5.90,
+      duration: '3 days',
+      startdate: '30.08.2020',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     }
    ];
